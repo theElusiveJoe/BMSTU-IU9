d532915130ee66c64675bb1bc1734e0224f9660f
theElusiveJoe <flvlimpo@gmail.com>
2022-04-20 15:24:33 +0300

    attempt to cjange algorythm

--- Computer Graphics/lab4.py
+++ Computer Graphics/lab4.py
@@ -8,7 +8,7 @@ from itertools import groupby
 
 display = None
 window = None
-resx, resy = 500, 500
+resx, resy = 800, 600
 
 delta = 0.1
 angle = 0.0
@@ -19,8 +19,9 @@ d_x, d_z = 30, 0
 # vertexes = [[50, 50], [490, 300], [100, 400]]
 # vertexes = [[100, 100],[120, 115], [100, 120], [90, 117]]
 # vertexes = [[100,100], [105,104], [100, 110], [95, 107]]
-# vertexes = [[100, 100], [200, 130], [210, 250], [150, 150], [110, 240]] 
-vertexes = [] #[[100,100], [150, 100], [150, 150], [200, 150], [200, 100], [250, 100], [250, 300], [100, 300]]
+# vertexes = [[100, 100], [200, 130], [210, 250], [150, 150], [110, 240]]
+# vertexes = [[100,100], [150, 100], [150, 150], [200, 150], [200, 100], [250, 100], [250, 300], [100, 300]]
+vertexes = [[100, 100], [200, 500]]
 bg_color = [1, 1, 1]
 edge_color = [1, 0, 0]
 vertex_color = [0, 0, 1]
@@ -67,13 +68,15 @@ def key_callback(window, key, scancode, action, mods):
         elif key == glfw.KEY_1:
             vertexes = [[50, 50], [490, 300], [100, 400]]
         elif key == glfw.KEY_2:
-            vertexes = [[100, 100],[120, 115], [100, 120], [90, 117]]
+            vertexes = [[100, 100], [120, 115], [100, 120], [90, 117]]
         elif key == glfw.KEY_3:
-            vertexes = [[100,100], [105,104], [100, 110], [95, 107]]
+            vertexes = [[100, 100], [105, 104], [100, 110], [95, 107]]
         elif key == glfw.KEY_4:
-            vertexes = [[100, 100], [200, 130], [210, 250], [150, 150], [110, 240]] 
+            vertexes = [[100, 100], [200, 130], [
+                210, 250], [150, 150], [110, 240]]
         elif key == glfw.KEY_5:
-            vertexes = [[100,100], [150, 100], [150, 150], [200, 150], [200, 100], [250, 100], [250, 300], [100, 300]]
+            vertexes = [[100, 100], [150, 100], [150, 150], [200, 150], [
+                200, 100], [250, 100], [250, 300], [100, 300]]
 
 
 def lines():
@@ -86,18 +89,13 @@ def lines():
         # print('START DRAWING EDGE')
         x1, y1, x2, y2 = vertex[0], vertex[1], vertexes[(
             i+1) % len(vertexes)][0], vertexes[(i+1) % len(vertexes)][1]
-        if x1 == x2: # вертикальные 
-            for j in range(min(y1, y2), max(y1,y2)+1):
+        if x1 == x2:  # вертикальные
+            for j in range(min(y1, y2), max(y1, y2)+1):
                 pixels[j][x1] = edge_color
-                # print('color', x1, j)
                 to_fill.append([x1, j])
-        elif y1 == y2: # горизонтальные 
-            print('YYYYY = ', y1)
+        elif y1 == y2:  # горизонтальные
             for j in range(min(x1, x2), max(x1, x2)+1):
                 pixels[y1][j] = edge_color
-                # print('color', j, y1) 
-            # to_fill.append([y1, x1])
-            # to_fill.append([y1, x1]) 
         else:
             dx = x2 - x1
             dy = y2 - y1
@@ -110,77 +108,33 @@ def lines():
             if dx > dy:
                 pdx, pdy = sign_x, 0
                 es, el = dy, dx
-                flag = False
             else:
                 pdx, pdy = 0, sign_y
                 es, el = dx, dy
-                flag = True
             x, y = x1, y1
             error, t = el/2, 0
 
-            # to_fill.append([x, y])
-            # for i in range(x, resx):
-            #     if pixels[y][i] == bg_color:
-            #         pixels[y][i] = fill_color
-            #     elif pixels[y][i] == fill_color:
-            #         pixels[y][i] = bg_color
-            # pixels[y][x] = edge_color
-            # to_fill.append([x, y])
-            # print('color', x, y)
+            tg = es/el
             while t < el:
+                print((error/es, error, es))
                 error -= es
                 if error < 0:
                     to_fill.append([x, y])
-                    # for i in range(x, resx):
-                    #     if pixels[y][i] == bg_color:
-                    #         pixels[y][i] = fill_color
-                    #     elif pixels[y][i] == fill_color:
-                    #         pixels[y][i] = bg_color
                     error += el
                     x += sign_x
                     y += sign_y
                 else:
                     if pdy != 0:
                         to_fill.append([x, y])
-                        # for i in range(x, resx):
-                        #     if pixels[y][i] == bg_color:
-                        #         pixels[y][i] = fill_color
-                        #     elif pixels[y][i] == fill_color:
-                        #         pixels[y][i] = bg_color
                     x += pdx
-                    y += pdy 
+                    y += pdy
                 t += 1
-                pixels[y][x] = edge_color
-                # print('color', x, y)
-        # print('END DRAWING EDGE')
+                pixels[y][x] = [abs(error/es), 0, 0]
 
-    # counter = defaultdict(int)
-    # for elem in to_fill:
-    #     counter[elem[1]] += 1
-    # to_check = [k for k in counter.keys() if counter[k]%2 == 1]
-    # print(to_check)
-    # vertX = []
-    # for xy in vertexes:
-    #     vertX.append(object)
-    # to_fill.sort(key=itemgetter(1))
-    # print('\n\n', [[y, [x[0] for x in xys]] for y, xys in groupby(to_fill, key=itemgetter(1))])
-    # to_fill = [[y, [x[0] for x in xys]] for y, xys in groupby(to_fill, key=itemgetter(1))]
-    # for i, pair in enumerate(to_fill):
-    #     if len(pair[1])%2 != 0:
-
-    # print(to_fill)
     edge_num = -1
-    # print('\n\n', vertexes)
     for i, xy in enumerate(to_fill):
         x, y = xy[0], xy[1]
-        # if xy in vertexes:
-        #     edge_num += 1
-        #     print('FOUND VERTEX:', x, y)
-        #     print('Y dir changes:', (vertexes[(edge_num+1)%len(vertexes)][1] - y) * (y - vertexes[(edge_num-1)%len(vertexes)][1]) < 0)
-        #     if (vertexes[(edge_num+1)%len(vertexes)][1] - y) * (y - vertexes[(edge_num-1)%len(vertexes)][1]) < 0:
-        #         continue
-        if (to_fill[(i+1)%len(to_fill)][1] - y) * (y - to_fill[(i-1)%len(to_fill)][1]) < 0:
-            # print('FOUND VERTEX:', x, y)
+        if (to_fill[(i+1) % len(to_fill)][1] - y) * (y - to_fill[(i-1) % len(to_fill)][1]) < 0:
             continue
         for i in range(x, resx):
             if pixels[y][i] == bg_color:
